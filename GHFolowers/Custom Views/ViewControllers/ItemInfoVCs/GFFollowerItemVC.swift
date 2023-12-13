7e3b2ca3f252f74ea71568729378ac99e58bb637
Joao Gripp <corguinha@gmail.com>
2023-12-13 10:24:48 +0000

    ItemInfoVC - SubClasses

---
+++ GHFolowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFFollowerItemVC.swift
+//  GHFolowers
+//
+//  Created by Joao Gripp on 13/12/23.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
---
+++ GHFolowers/Custom Views/ViewControllers/ItemInfoVCs/GFRepoItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFRepoItemVC.swift
+//  GHFolowers
+//
+//  Created by Joao Gripp on 13/12/23.
+//
+
+import UIKit
+
+class GFRepoItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "GitHub Profile")
+    }
+}

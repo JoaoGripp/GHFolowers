0c429a57863e9e951be7b44ba125484353832aba
Joao Gripp <corguinha@gmail.com>
2024-01-16 09:40:45 +0000

    Clean Up - GFAlertVC, Convenience Init, DateDecoding, Networking Refactor

--- GHFolowers/Custom Views/Views/GFAlertContainerView.swift
+++ GHFolowers/Custom Views/Views/GFAlertContainerView.swift
@@ -1,31 +1,31 @@
 //
-//  GFAvatarImageView.swift
+//  GFAlertContainerView.swift
 //  GHFolowers
 //
-//  Created by Joao Gripp on 08/12/23.
+//  Created by Joao Gripp on 16/01/24.
 //
 
 import UIKit
 
-class GFAvatarImageView: UIImageView {
-    
-    let cache = NetworkManager.shared.cache
-    let placeholderImage = UIImage(named: "avatar-placeholder")!
+class GFAlertContainerView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         configure()
     }
     
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    
     private func configure() {
-        layer.cornerRadius = 10
-        clipsToBounds = true
-        image = placeholderImage
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
         translatesAutoresizingMaskIntoConstraints = false
     }
-
+    
 }

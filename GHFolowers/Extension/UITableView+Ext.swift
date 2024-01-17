579261a8120ef53e72b3a9485cb2481a8324954e
Joao Gripp <corguinha@gmail.com>
2024-01-17 13:50:18 +0000

    Clean Up - Persistence Manager, UITableView Extension & Bug Fixes

---
+++ GHFolowers/Extension/UITableView+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  UITableView+Ext.swift
+//  GHFolowers
+//
+//  Created by Joao Gripp on 17/01/24.
+//
+
+import UIKit
+
+extension UITableView {
+    
+    func reloadDataOnMainThread() {
+        DispatchQueue.main.async {
+            self.reloadData()
+        }
+    }
+    
+    func removeExcessCells() {
+        tableFooterView = UIView(frame: .zero)
+    }
+}
